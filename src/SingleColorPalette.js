ef7e606a71903f1f916db59866e9678a143ae800
jump3rhood <mjm13121@gmail.com>
2021-06-08 20:05:33 +0530

    Add Navbar and Footer to SingleColorPalette

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
+import Navbar from './Navbar';
 import ColorBox from './ColorBox';
+import PaletteFooter from './PaletteFooter'
 class SingleColorPalette extends Component {
     constructor(props) {
         super(props);
         this._shades = this.gatherShades(this.props.palette, this.props.colorId);
+        this.state = { format: "hex" };
+        this.changeFormat = this.changeFormat.bind(this);
     }
     gatherShades(palette, colorToFilterBy) {
         let shades = [];
@@ -14,20 +18,26 @@ class SingleColorPalette extends Component {
         // gather the shades of the color with the colorId
         return shades.slice(1);
     }
+    changeFormat(value) {
+        this.setState({ format: value });
+    }
     render() {
+        const { format } = this.state;
+        const { paletteName, emoji } = this.props.palette;
         const colorBoxes = this._shades.map(color => (
             <ColorBox key={color.name}
                 name={color.name}
-                background={color.hex}
+                background={color[format]}
                 showLink={false}
             />
         ))
         return (
             <div className="Palette">
-                <h1>Single Color Page</h1>
+                <Navbar handleChange={this.changeFormat} showingAllColors={false} />
                 <div className="Palette-colors">
                     {colorBoxes}
                 </div>
+                <PaletteFooter paletteName={paletteName} emoji={emoji} />
             </div>
         )
     }
