726588c1802bafe2cc15795b8e6c2d5a5355e7bc
jump3rhood <mjm13121@gmail.com>
2021-06-08 22:35:35 +0530

    Adjust text color based on luminance

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import chroma from 'chroma-js';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { Link } from 'react-router-dom';
 import './ColorBox.css';
@@ -16,6 +17,8 @@ class ColorBox extends Component {
     render() {
         const { name, background, moreUrl, showLink } = this.props;
         const { copied } = this.state;
+        const isDarkColor = chroma(background).luminance() <= 0.08;
+        const isLightColor = chroma(background).luminance() > 0.7;
         return (
             <CopyToClipboard text={background} onCopy={this.changeCopyState}>
                 <div className="ColorBox" style={{ background: background }}>
@@ -24,19 +27,19 @@ class ColorBox extends Component {
                         style={{ background: background }} />
                     <div className={`copy-msg ${copied && "show"}`}>
                         <h1>copied!</h1>
-                        <p>{background}</p>
+                        <p className={isLightColor && 'dark-text'}>{background}</p>
                     </div>
                     <div className="copy-container">
                         <div className="box-content">
-                            <span>{name}</span>
+                            <span className={isDarkColor && "light-text"}>{name}</span>
                         </div>
-                        <button className="copy-button">
+                        <button className={`copy-button ${isLightColor && 'dark-text'}`}>
                             Copy
                     </button>
                     </div>
                     {showLink &&
                         (<Link to={moreUrl} onClick={e => e.stopPropagation()}>
-                            <span className="see-more">MORE</span>
+                            <span className={`see-more ${isLightColor && 'dark-text'}`}>MORE</span>
                         </Link>)
                     }
 
